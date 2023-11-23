70927009a83f6f776eb6f33a1868f1e792b095ed
alena.bushkina <alena.bushkina@vk.team>
2023-11-24 02:17:33 +0300

    create Application.class

--- src/main/java/com/cygans/Application.java
+++ src/main/java/com/cygans/Application.java
@@ -1,13 +1,22 @@
 package com.cygans;
 
+import com.vaadin.flow.component.dependency.NpmPackage;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.vaadin.artur.helpers.LaunchUtil;
 
 /**
  * Точка входа в Spring Boot application
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, ErrorMvcAutoConfiguration.class})
+@NpmPackage(value = "@fontsource/ubuntu", version = "4.5.0")
+@NpmPackage(value = "lumo-css-framework", version = "^4.0.10")
+@NpmPackage(value = "line-awesome", version = "1.3.0")
 public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
+        LaunchUtil.launchBrowserInDevelopmentMode(SpringApplication.run(Application.class, args));
     }
 }
