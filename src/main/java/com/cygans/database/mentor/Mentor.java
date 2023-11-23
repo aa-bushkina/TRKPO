00452fd10de610182e7953f7030d6fcb69633a84
alena.bushkina <alena.bushkina@vk.team>
2023-11-24 02:47:34 +0300

    create Mentor model extended Person

---
+++ src/main/java/com/cygans/database/mentor/Mentor.java
@@ -0,0 +1,45 @@
+package com.cygans.database.mentor;
+
+import com.cygans.database.person.Person;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.time.LocalDate;
+
+
+@Entity
+@Table(name = "mentor")
+public class Mentor extends Person {
+    public Mentor(
+            String firstName,
+            String lastName,
+            String login,
+            String phone,
+            String gender,
+            LocalDate birthday,
+            Long loginInfoId) {
+        this.firstname = firstName;
+        this.lastname = lastName;
+        this.login = login;
+        this.phone = phone;
+        this.gender = gender;
+        this.birthday = birthday;
+        this.loginInfoId = loginInfoId;
+    }
+
+    public Mentor() {
+    }
+
+    @Override
+    public String toString() {
+        return "Mentor {" +
+                "id=" + id +
+                ", firstName='" + firstname +
+                ", lastName='" + lastname +
+                ", login='" + login +
+                ", phone='" + phone +
+                ", gender='" + gender +
+                ", birthday=" + birthday +
+                '}';
+    }
+}
