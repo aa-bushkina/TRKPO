ddaf458f3c7f0acb806ea9f635997da0e7fd245f
stolbovik <stolbov_svyatoslav_02@mail.ru>
2023-11-24 21:10:36 +0300

    add Intensity

---
+++ src/main/java/com/cygans/database/sport_log/lntensity/Intensity.java
@@ -0,0 +1,35 @@
+package com.cygans.database.sport_log.lntensity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "intensity")
+public class Intensity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "type")
+    private String type;
+
+    public Intensity() {}
+    public Intensity(String type) {
+        this.type = type;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+}
