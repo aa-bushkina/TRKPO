4965ce96476e9521f937cb14a6215f4143e07530
alena.bushkina <alena.bushkina@vk.team>
2023-11-28 03:09:07 +0300

    add empty view classes

---
+++ src/main/java/com/cygans/views/SignUpRolesView.java
@@ -0,0 +1,7 @@
+package com.cygans.views;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+
+public class SignUpRolesView extends VerticalLayout {
+
+}
---
+++ src/main/java/com/cygans/views/participant/signup/ParticipantSignUp3View.java
@@ -0,0 +1,10 @@
+package com.cygans.views.participant.signup;
+
+import com.vaadin.flow.component.html.Div;
+
+/**
+ * Страница регистрации участника 3
+ */
+public class ParticipantSignUp3View extends Div {
+
+}
