e71a7fbab87fd681b270ce2899cd8e0e87967866
stolbovik <stolbov_svyatoslav_02@mail.ru>
2023-11-29 00:26:48 +0300

    add ParticipantPersonData

---
+++ src/main/java/com/cygans/views/participant/logbooks/ParticipantPersonData.java
@@ -0,0 +1,29 @@
+package com.cygans.views.participant.logbooks;
+
+import java.time.LocalDate;
+
+public class ParticipantPersonData {
+    private LocalDate date;
+    private String logBookType;
+    private long logBookId;
+
+    public String getLogBookType(){
+        return logBookType;
+    }
+    public void setLogBookType(String LogBokType){
+        this.logBookType = LogBokType;
+    }
+    public Long getLogBookId(){
+        return logBookId;
+    }
+    public void setLogBookId(Long logBookId){
+        this.logBookId = logBookId;
+    }
+    public void setDate(LocalDate date){
+        this.date =date;
+    }
+    public LocalDate getDate(){
+        return date;
+    }
+
+}
