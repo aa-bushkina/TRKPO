35a190a3db1d9508897a583f41c31654ea4a3e2c
Nirvana <svaikinilya@yandex.ru>
2023-12-04 03:25:41 +0300

    fix passwds

--- src/main/java/com/cygans/views/util/Filler.java
+++ src/main/java/com/cygans/views/util/Filler.java
@@ -162,19 +162,19 @@ public class Filler {
     long partId_3 = -1;
 
     if (mentorService.isNeedToAddHardcodedUser()) {
-      mentId_0 = createMentor("Ванька", "Головин", "vanya", "Vanvanvan", "89137197445");
-      mentId_1 = createMentor("Лёшка", "Бойкин", "alexPro", "fox21century", "89137197445");
-      mentId_2 = createMentor("Илюшка", "Работин", "ilya", "reddead", "89137197445");
-      mentId_3 = createMentor("Фиона", "Павлова", "phionPAV", "alpachino", "89137197445");
+      mentId_0 = createMentor("Ванька", "Головин", "vanya", "SecretPassword1", "89137197445");
+      mentId_1 = createMentor("Лёшка", "Бойкин", "alexPro", "Myst3ry_123", "89137197445");
+      mentId_2 = createMentor("Илюшка", "Работин", "ilya", "N1nja_Parad1se", "89137197445");
+      mentId_3 = createMentor("Фиона", "Павлова", "phionPAV", "ChocoLab_88", "89137197445");
       mentid = createMentor("2", "2", "2", "2", "2");
     }
     boolean f = false;
     if (participantService.isNeedToAddHardcodedUser()) {
       f = true;
-      partId_0 = createParticipant("Катька", "Волосова", "katya", "katkatkat", "89383170126");
-      partId_1 = createParticipant("Петька", "Совкин", "petka_super", "tarakan1", "89358127132");
-      partId_2 = createParticipant("Наташка", "Мирянцева", "natashaNataly", "leonardo", "89217132831");
-      partId_3 = createParticipant("Олежка", "Питулин", "olegBoss777", "telefon", "89991833614");
+      partId_0 = createParticipant("Катька", "Волосова", "katya", "Qu_ntum_42", "89383170126");
+      partId_1 = createParticipant("Петька", "Совкин", "petka_super", "G0ld3nSun_7", "89358127132");
+      partId_2 = createParticipant("Наташка", "Мирянцева", "natashaNataly", "S_vvyPanda_9", "89217132831");
+      partId_3 = createParticipant("Олежка", "Питулин", "olegBoss777", "V0ltage_Star", "89991833614");
       partid = createParticipant("1", "1", "1", "1", "1");
     }
     if (participantMentorService.isNeedToConnectUsers(mentId_0, partId_0)) {
