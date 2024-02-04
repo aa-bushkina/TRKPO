9a1685f7d333a935ddd9525515d144719735d63f
stolbovik <stolbov_svyatoslav_02@mail.ru>
2024-02-04 19:05:40 +0300

    add 2 tests for Notifications

---
+++ src/test/java/TestsNotifications/TestsGetNotificationId.java
@@ -0,0 +1,17 @@
+package TestsNotifications;
+
+import com.cygans.database.notifications.Notifications;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestsGetNotificationId {
+
+    //Проверяет, что изначельно геттер выдает 0
+    @Test
+    public void testGetNotificationIWithoutSet() {
+        Notifications notifications = new Notifications(1L, 2L, 3L, 4L);
+        assertEquals(0, notifications.getNotificationId());
+    }
+
+}
