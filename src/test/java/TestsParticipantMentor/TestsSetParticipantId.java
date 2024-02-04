bf8a6282488b444712b513692a204f4f2d1930d0
stolbovik <stolbov_svyatoslav_02@mail.ru>
2024-02-04 22:54:10 +0300

    add tests for ParticipantMentor

---
+++ src/test/java/TestsParticipantMentor/TestsSetParticipantId.java
@@ -0,0 +1,25 @@
+package TestsParticipantMentor;
+
+import com.cygans.database.participant_mentor.ParticipantMentor;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class TestsSetParticipantId {
+
+    private static final Long PARTICIPANT_ID_BEFORE = 2L;
+    private static final Long PARTICIPANT_ID_AFTER = 10L;
+
+    /**
+     * Проверяет работу setParticipantId
+     */
+    @Test
+    public void testSetQuestionId() {
+        ParticipantMentor participantMentor = new ParticipantMentor(PARTICIPANT_ID_BEFORE, 2L);
+        assertEquals(PARTICIPANT_ID_BEFORE, participantMentor.getParticipantId(), "Изначально неверное значение");
+        participantMentor.setParticipantId(PARTICIPANT_ID_AFTER);
+        assertEquals(PARTICIPANT_ID_AFTER, participantMentor.getParticipantId(), "Установилось неверное значение");
+    }
+
+}
