26e00aac0b1bfb5dcb67f989ba8772543ad847c1
stolbovik <stolbov_svyatoslav_02@mail.ru>
2024-02-05 00:36:20 +0300

    add tests for Person

---
+++ src/test/java/TestsPerson/TestsGetGender.java
@@ -0,0 +1,20 @@
+package TestsPerson;
+
+import com.cygans.database.Person;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestsGetGender {
+
+    /**
+     * Проверка работы getGender
+     */
+    @Test
+    public void testGetGender() {
+        Person person = new PersonForTest();
+        person.setGender("Male");
+        assertEquals("Male", person.getGender(), "Вернулось неверное значнение gender");
+    }
+
+}
---
+++ src/test/java/TestsPerson/TestsGetLogin.java
@@ -0,0 +1,20 @@
+package TestsPerson;
+
+import com.cygans.database.Person;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestsGetLogin {
+
+    /**
+     * Проверяет работы getLogin
+     */
+    @Test
+    public void testGetLogin() {
+        Person person = new PersonForTest();
+        person.setLogin("john.doe@example.com");
+        assertEquals("john.doe@example.com", person.getLogin(), "Верулся неверный логин");
+    }
+
+}
