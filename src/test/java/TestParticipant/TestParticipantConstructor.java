9948ea17e7f98403b41ed5efd1e5ab76f0add60f
Nirvana <svaikinilya@yandex.ru>
2024-02-04 23:44:11 +0300

    test constuctor and to_string

---
+++ src/test/java/TestParticipant/TestParticipantConstructor.java
@@ -0,0 +1,99 @@
+package TestParticipant;
+
+import com.cygans.database.mentor.Mentor;
+import com.cygans.database.participant.Participant;
+import com.fasterxml.jackson.databind.node.BigIntegerNode;
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Проверяет конструктор
+ */
+public class TestParticipantConstructor {
+
+  private static final String FIRST_NAME = "Olga";
+  private static final String LAST_NAME = "Petrova";
+  private static final String LOGIN = "olga-petrova";
+  private static final String PHONE = "+79615623177";
+  private static final String GENDER = "Female";
+  private static final LocalDate BIRTHDAY = LocalDate.of(1978, 1, 21);
+  private static final Integer HEIGHT = 120;
+  private static final Integer WEIGHT = 120;
+  private static final Integer HIPS = 120;
+  private static final Integer WAIST = 120;
+  private static final Integer BREAST = 120;
+  private static final Long LOGIN_INFO_ID = 1L;
+
+
+  /**
+   * Проверяет конструктор с параметрами
+   */
+  @Test
+  public void testConstructorWithParameters() {
+    Participant participant = new Participant(FIRST_NAME, LAST_NAME, LOGIN, PHONE, GENDER, BIRTHDAY, HEIGHT,
+                                                                WEIGHT, HIPS, WAIST, BREAST, LOGIN_INFO_ID);
+
+    assertAll(
+      () -> assertEquals(FIRST_NAME, participant.getFirstName()),
+      () -> assertEquals(LAST_NAME, participant.getLastName()),
+      () -> assertEquals(LOGIN, participant.getLogin()),
+      () -> assertEquals(PHONE, participant.getPhone()),
+      () -> assertEquals(GENDER, participant.getGender()),
+      () -> assertEquals(BIRTHDAY, participant.getBirthday()),
+      () -> assertEquals(HEIGHT, participant.getHeight()),
+      () -> assertEquals(WEIGHT, participant.getWeight()),
+      () -> assertEquals(HIPS, participant.getHips()),
+      () -> assertEquals(WAIST, participant.getWaist()),
+      () -> assertEquals(BREAST, participant.getBreast()),
+      () -> assertEquals(LOGIN_INFO_ID, participant.getLoginInfoId())
+    );
+  }
+
+  /**
+   * Проверяет конструктор без параметров
+   */
+  @Test
+  public void testDefaultConstructor() {
+    Participant participant = new Participant();
+
+    assertAll(
+      () -> assertNull(participant.getFirstName()),
+      () -> assertNull(participant.getLastName()),
+      () -> assertNull(participant.getLogin()),
+      () -> assertNull(participant.getPhone()),
+      () -> assertNull(participant.getGender()),
+      () -> assertNull(participant.getBirthday()),
+      () -> assertNull(participant.getHeight()),
+      () -> assertNull(participant.getWeight()),
+      () -> assertNull(participant.getHips()),
+      () -> assertNull(participant.getWaist()),
+      () -> assertNull(participant.getBreast()),
+      () -> assertNull(participant.getLoginInfoId())
+    );
+  }
+
+  /**
+   * Проверяет конструктор с параметрами null
+   */
+  @Test
+  public void testConstructorWithNullValues() {
+    assertThrows(IllegalArgumentException.class, () -> new Participant(null, null, null, null, null, null, null, null, null, null, null, null),
+      "Не получили ожидаеме исключение при вызове метода со всеми параметрами null");
+  }
+
+  /**
+   * Проверяет, что конструктор обрабатывает случай с недостаточным количеством значений
+   */
+  @Test
+  public void testConstructorWithMissingRequiredValues() {
+    assertThrows(IllegalArgumentException.class, () -> new Participant(FIRST_NAME, LAST_NAME, null, null, GENDER, null, null, null, null, null, null, LOGIN_INFO_ID ),
+      "Не получили ожидаеме исключение при вызове метода без всех обязательных параметров");
+
+    assertThrows(IllegalArgumentException.class, () ->  new Participant("", "", "", PHONE, GENDER, BIRTHDAY, HEIGHT,
+      WEIGHT, HIPS, WAIST, BREAST, LOGIN_INFO_ID),
+      "Не получили ожидаеме исключение при вызове метода с пустыми обязательными параметрами");
+  }
+}
---
+++ src/test/java/TestParticipant/TestParticipantToString.java
@@ -0,0 +1,186 @@
+package TestParticipant;
+
+import com.cygans.database.mentor.Mentor;
+import com.cygans.database.participant.Participant;
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Провеяет метод toString()
+ */
+public class TestParticipantToString {
+    private static final String FIRST_NAME = "Olga";
+    private static final String FIRST_NAME_UPDATED = "Ola";
+    private static final String LAST_NAME = "Petrovna";
+    private static final String LAST_NAME_UPDATED = "Petrova";
+    private static final String LOGIN = "olga-petrova";
+    private static final String LOGIN_UPDATED = "olga-petrova@ola";
+    private static final String PHONE = "+79615623177";
+    private static final String PHONE_UPDATED = "+79615623199";
+    private static final String GENDER = "Female";
+    private static final String GENDER_UPDATED = "Male";
+    private static final LocalDate BIRTHDAY = LocalDate.of(1978, 1, 21);
+    private static final LocalDate BIRTHDAY_UPDATED = BIRTHDAY.plusYears(1);
+    private static final Integer HEIGHT = 120;
+    private static final Integer HEIGHT_UPDATED = 110;
+    private static final Integer WEIGHT = 120;
+    private static final Integer WEIGHT_UPDATED = 110;
+    private static final Integer HIPS = 120;
+    private static final Integer HIPS_UPDATED = 110;
+    private static final Integer WAIST = 120;
+    private static final Integer WAIST_UPDATED = 110;
+    private static final Integer BREAST = 120;
+    private static final Integer BREAST_UPDATED = 110;
+    private static final Long LOGIN_INFO_ID = 1L;
+
+    private static final String HEIGHT_FIELD = "height=";
+    private static final String WEIGHT_FIELD = "weight=";
+    private static final String HIPS_FILED = "hips=";
+    private static final String WAIST_FIELD = "waist=";
+    private static final String BREAST_FIELD = "breast=";
+    private static final String ID_FIELD = "id=";
+    private static final String FIRST_NAME_FIELD = "firstname=";
+    private static final String LAST_NAME_FIELD = "lastname=";
+    private static final String LOGIN_FIELD = "login=";
+    private static final String PHONE_FIELD = "phone=";
+    private static final String GENDER_FIELD = "gender=";
+    private static final String BIRTHDAY_FIELD = "birthday=";
+    private static final String LOGIN_INFO_ID_FIELD = "loginInfoId=";
+
+    private static final String NULL_STRING = "null";
+
+    /**
+     * Проверяет метод toString() на наличие обязательных полей
+     */
+    @Test
+    public void testToStringWithRequiredFields() {
+        Participant participant = new Participant(FIRST_NAME, LAST_NAME, LOGIN, PHONE, GENDER, BIRTHDAY, HEIGHT,
+          WEIGHT, HIPS, WAIST, BREAST, LOGIN_INFO_ID);
+
+        String toStringResult = participant.toString();
+        assertAll(
+              () -> assertTrue(toStringResult.contains(HEIGHT_FIELD)),
+              () -> assertTrue(toStringResult.contains(WEIGHT_FIELD)),
+              () -> assertTrue(toStringResult.contains(HIPS_FILED)),
+              () -> assertTrue(toStringResult.contains(WAIST_FIELD)),
+              () -> assertTrue(toStringResult.contains(BREAST_FIELD)),
+              () -> assertTrue(toStringResult.contains(ID_FIELD)),
+              () -> assertTrue(toStringResult.contains(FIRST_NAME_FIELD)),
+              () -> assertTrue(toStringResult.contains(LAST_NAME_FIELD)),
+              () -> assertTrue(toStringResult.contains(LOGIN_FIELD)),
+              () -> assertTrue(toStringResult.contains(PHONE_FIELD)),
+              () -> assertTrue(toStringResult.contains(GENDER_FIELD)),
+              () -> assertTrue(toStringResult.contains(BIRTHDAY_FIELD)),
+              () -> assertTrue(toStringResult.contains(LOGIN_INFO_ID_FIELD))
+        );
+    }
+
+    /**
+     * Провеяет метод toString() с валидными значениями полей
+     */
+    @Test
+    public void testToString() {
+      Participant participant = new Participant(FIRST_NAME, LAST_NAME, LOGIN, PHONE, GENDER, BIRTHDAY, HEIGHT,
+        WEIGHT, HIPS, WAIST, BREAST, LOGIN_INFO_ID);
+
+        String toStringResult = participant.toString();
+        assertAll(
+          () -> assertTrue(toStringResult.contains(HEIGHT_FIELD + HEIGHT)),
+          () -> assertTrue(toStringResult.contains(WEIGHT_FIELD + WEIGHT)),
+          () -> assertTrue(toStringResult.contains(HIPS_FILED + HIPS)),
+          () -> assertTrue(toStringResult.contains(WAIST_FIELD + WAIST)),
+          () -> assertTrue(toStringResult.contains(BREAST_FIELD + BREAST)),
+          () -> assertTrue(toStringResult.contains(ID_FIELD + 0)),
+          () -> assertTrue(toStringResult.contains(FIRST_NAME_FIELD + FIRST_NAME)),
+          () -> assertTrue(toStringResult.contains(LAST_NAME_FIELD + LAST_NAME)),
+          () -> assertTrue(toStringResult.contains(LOGIN_FIELD + LOGIN)),
+          () -> assertTrue(toStringResult.contains(PHONE_FIELD + PHONE)),
+          () -> assertTrue(toStringResult.contains(GENDER_FIELD + GENDER)),
+          () -> assertTrue(toStringResult.contains(BIRTHDAY_FIELD + BIRTHDAY)),
+          () -> assertTrue(toStringResult.contains(LOGIN_INFO_ID_FIELD + LOGIN_INFO_ID))
+        );
+    }
+
+    /**
+     * Проверяет метод toString() после изменений полей объекта
+     */
+    @Test
+    public void testToStringAfterModifications() {
+        Participant participant = new Participant(FIRST_NAME, LAST_NAME, LOGIN, PHONE, GENDER, BIRTHDAY, HEIGHT,
+          WEIGHT, HIPS, WAIST, BREAST, LOGIN_INFO_ID);
+        String toStringResult = participant.toString();
+        assertAll(
+          () -> assertTrue(toStringResult.contains(HEIGHT_FIELD + HEIGHT)),
+          () -> assertTrue(toStringResult.contains(WEIGHT_FIELD + WEIGHT)),
+          () -> assertTrue(toStringResult.contains(HIPS_FILED + HIPS)),
+          () -> assertTrue(toStringResult.contains(WAIST_FIELD + WAIST)),
+          () -> assertTrue(toStringResult.contains(BREAST_FIELD + BREAST)),
+          () -> assertTrue(toStringResult.contains(ID_FIELD + 0)),
+          () -> assertTrue(toStringResult.contains(FIRST_NAME_FIELD + FIRST_NAME)),
+          () -> assertTrue(toStringResult.contains(LAST_NAME_FIELD + LAST_NAME)),
+          () -> assertTrue(toStringResult.contains(LOGIN_FIELD + LOGIN)),
+          () -> assertTrue(toStringResult.contains(PHONE_FIELD + PHONE)),
+          () -> assertTrue(toStringResult.contains(GENDER_FIELD + GENDER)),
+          () -> assertTrue(toStringResult.contains(BIRTHDAY_FIELD + BIRTHDAY)),
+          () -> assertTrue(toStringResult.contains(LOGIN_INFO_ID_FIELD + LOGIN_INFO_ID))
+        );
+
+        participant.setFirstName(FIRST_NAME_UPDATED);
+        participant.setLastName(LAST_NAME_UPDATED);
+        participant.setLogin(LOGIN_UPDATED);
+        participant.setPhone(PHONE_UPDATED);
+        participant.setGender(GENDER_UPDATED);
+        participant.setBirthday(BIRTHDAY_UPDATED);
+        participant.setBreast(BREAST_UPDATED);
+        participant.setHeight(HEIGHT_UPDATED);
+        participant.setHips(HIPS_UPDATED);
+        participant.setWaist(WAIST_UPDATED);
+        participant.setWeight(WEIGHT_UPDATED);
+
+        String toStringResultUpdated = participant.toString();
+        assertAll(
+          () -> assertTrue(toStringResultUpdated.contains(HEIGHT_FIELD + HEIGHT_UPDATED)),
+          () -> assertTrue(toStringResultUpdated.contains(WEIGHT_FIELD + WEIGHT_UPDATED)),
+          () -> assertTrue(toStringResultUpdated.contains(HIPS_FILED + HIPS_UPDATED)),
+          () -> assertTrue(toStringResultUpdated.contains(WAIST_FIELD + WAIST_UPDATED)),
+          () -> assertTrue(toStringResultUpdated.contains(BREAST_FIELD + BREAST_UPDATED)),
+          () -> assertTrue(toStringResultUpdated.contains(ID_FIELD + 0)),
+          () -> assertTrue(toStringResultUpdated.contains(FIRST_NAME_FIELD + FIRST_NAME_UPDATED)),
+          () -> assertTrue(toStringResultUpdated.contains(LAST_NAME_FIELD + LAST_NAME_UPDATED)),
+          () -> assertTrue(toStringResultUpdated.contains(LOGIN_FIELD + LOGIN_UPDATED)),
+          () -> assertTrue(toStringResultUpdated.contains(PHONE_FIELD + PHONE_UPDATED)),
+          () -> assertTrue(toStringResultUpdated.contains(GENDER_FIELD + GENDER_UPDATED)),
+          () -> assertTrue(toStringResultUpdated.contains(BIRTHDAY_FIELD + BIRTHDAY_UPDATED)),
+          () -> assertTrue(toStringResultUpdated.contains(LOGIN_INFO_ID_FIELD + LOGIN_INFO_ID))
+        );
+    }
+
+    /**
+     * Проверяет метод toString() с пустыми значениями полей
+     */
+    @Test
+    public void testToStringWithEmptyFields() {
+        Participant participant = new Participant();
+
+        String toStringResult = participant.toString();
+        assertAll(
+          () -> assertTrue(toStringResult.contains(HEIGHT_FIELD + NULL_STRING)),
+          () -> assertTrue(toStringResult.contains(WEIGHT_FIELD + NULL_STRING)),
+          () -> assertTrue(toStringResult.contains(HIPS_FILED + NULL_STRING)),
+          () -> assertTrue(toStringResult.contains(WAIST_FIELD + NULL_STRING)),
+          () -> assertTrue(toStringResult.contains(BREAST_FIELD + NULL_STRING)),
+          () -> assertTrue(toStringResult.contains(ID_FIELD + 0)),
+          () -> assertTrue(toStringResult.contains(FIRST_NAME_FIELD + NULL_STRING)),
+          () -> assertTrue(toStringResult.contains(LAST_NAME_FIELD + NULL_STRING)),
+          () -> assertTrue(toStringResult.contains(LOGIN_FIELD + NULL_STRING)),
+          () -> assertTrue(toStringResult.contains(PHONE_FIELD + NULL_STRING)),
+          () -> assertTrue(toStringResult.contains(GENDER_FIELD + NULL_STRING)),
+          () -> assertTrue(toStringResult.contains(BIRTHDAY_FIELD + NULL_STRING)),
+          () -> assertTrue(toStringResult.contains(LOGIN_INFO_ID_FIELD + NULL_STRING))
+        );
+    }
+}
