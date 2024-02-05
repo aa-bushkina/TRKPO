a78985c67612de0706e2cb037e0156723fae137e
stolbovik <stolbov_svyatoslav_02@mail.ru>
2024-02-05 21:30:31 +0300

    add tests for NotificationTypeService

---
+++ src/test/java/TestsNotifications/TestsNotificationTypeModel/TestsGetId.java
@@ -0,0 +1,20 @@
+package TestsNotifications.TestsNotificationTypeModel;
+
+import com.cygans.database.notifications.notification_type.NotificationType;
+import com.cygans.database.notifications.notification_type.TypeOfNotification;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestsGetId {
+
+    /**
+     * Проверяет работу getId
+     */
+    @Test
+    public void testGetId() {
+        NotificationType notificationType = new NotificationType(TypeOfNotification.ANSWER_ON_QUESTION.getValue());
+        assertNull(notificationType.getId(), "ID не null");
+    }
+
+}
---
+++ src/test/java/TestsNotifications/TestsNotificationsModel/TestsSetNotificationTypeId.java
@@ -0,0 +1,24 @@
+package TestsNotifications.TestsNotificationsModel;
+
+import com.cygans.database.notifications.Notifications;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestsSetNotificationTypeId {
+
+    private static final Long NOTIFICATION_TYPE_ID_BEFORE = 10L;
+    private static final Long NOTIFICATION_TYPE_ID_AFTER = 11L;
+
+    /**
+     * Проверяет работу setNotificationTypeId
+     */
+    @Test
+    public void testSetNotificationTypeId() {
+        Notifications notifications = new Notifications(1L, 1L, NOTIFICATION_TYPE_ID_BEFORE, 1L);
+        assertEquals(NOTIFICATION_TYPE_ID_BEFORE, notifications.getNotificationTypeId(), "Изначально неверное значение");
+        notifications.setNotificationTypeId(NOTIFICATION_TYPE_ID_AFTER);
+        assertEquals(NOTIFICATION_TYPE_ID_AFTER, notifications.getNotificationTypeId(), "Установилось неверное значение");
+    }
+
+}
