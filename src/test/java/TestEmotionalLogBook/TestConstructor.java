88e01475be3e88d1e31fb5cb65b48ce75112663b
alena.bushkina <alena.bushkina@vk.team>
2024-02-05 03:40:52 +0300

    add tests for TestConstructor in TestEmotionalLogBook

--- src/test/java/TestEmotionalLogBook/TestConstructor.java
+++ src/test/java/TestEmotionalLogBook/TestConstructor.java
@@ -13,60 +13,86 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TestConstructor {
 
-  private static final Long LOG_BOOK_ID = 5L;
-  private static final LocalDateTime LOCAL_DATE_TIME = LocalDateTime.now();
-  private static final String DESCRIPTION = "Emotional text.";
+    private static final Long LOG_BOOK_ID = 5L;
+    private static final LocalDateTime LOCAL_DATE_TIME = LocalDateTime.now();
+    private static final String DESCRIPTION = "Emotional text.";
 
-  /**
-   * Проверяет конструктор с параметрами
-   */
-  @Test
-  public void testConstructorWithParameters() {
-    EmotionalLogBook emotionalLogBook = new EmotionalLogBook(LOG_BOOK_ID, LOCAL_DATE_TIME, DESCRIPTION);
+    /**
+     * Проверяет конструктор с параметрами
+     */
+    @Test
+    public void testConstructorWithParameters() {
+        EmotionalLogBook emotionalLogBook = new EmotionalLogBook(LOG_BOOK_ID, LOCAL_DATE_TIME, DESCRIPTION);
 
-    assertAll(
-      () -> assertEquals(LOG_BOOK_ID, emotionalLogBook.getLogBookId()),
-      () -> assertEquals(LOCAL_DATE_TIME, emotionalLogBook.getTimeType()),
-      () -> assertEquals(DESCRIPTION, emotionalLogBook.getDescription())
-    );
-  }
+        assertAll(
+                () -> assertEquals(LOG_BOOK_ID, emotionalLogBook.getLogBookId()),
+                () -> assertEquals(LOCAL_DATE_TIME, emotionalLogBook.getTimeType()),
+                () -> assertEquals(DESCRIPTION, emotionalLogBook.getDescription())
+        );
+    }
 
-  /**
-   * Проверяет конструктор без параметров
-   */
-  @Test
-  public void testDefaultConstructor() {
-    EmotionalLogBook emotionalLogBook = new EmotionalLogBook();
+    /**
+     * Проверяет конструктор без параметров
+     */
+    @Test
+    public void testDefaultConstructor() {
+        EmotionalLogBook emotionalLogBook = new EmotionalLogBook();
 
-    assertAll(
-      () -> assertNotNull(emotionalLogBook.getId()),
-      () -> assertNull(emotionalLogBook.getLogBookId()),
-      () -> assertNull(emotionalLogBook.getDescription()),
-      () -> assertNull(emotionalLogBook.getTimeType())
-    );
-  }
+        assertAll(
+                () -> assertNotNull(emotionalLogBook.getId()),
+                () -> assertNull(emotionalLogBook.getLogBookId()),
+                () -> assertNull(emotionalLogBook.getDescription()),
+                () -> assertNull(emotionalLogBook.getTimeType())
+        );
+    }
 
-  /**
-   * Проверяет конструктор с параметрами null
-   */
-  @Test
-  public void testConstructorWithNullValues() {
-    assertThrows(IllegalArgumentException.class, () -> new EmotionalLogBook(null, null, null),
-      "Не получили ожидаеме исключение при вызове метода со всеми параметрами null");
-  }
+    /**
+     * Проверяет конструктор с параметрами null
+     */
+    @Test
+    public void testConstructorWithNullValues() {
+        assertThrows(IllegalArgumentException.class, () -> new EmotionalLogBook(null, null, null),
+                "Не получили ожидаеме исключение при вызове метода со всеми параметрами null");
+    }
 
-  /**
-   * Проверяет, что конструктор обрабатывает случай с недостаточным количеством значений
-   */
-  @Test
-  public void testConstructorWithMissingRequiredValues() {
-    assertThrows(IllegalArgumentException.class, () -> new EmotionalLogBook(null, null, DESCRIPTION),
-      "Не получили ожидаеме исключение при вызове метода без всех обязательных параметров");
+    /**
+     * Проверяет конструктор с параметром LogBookId = null
+     */
+    @Test
+    public void testConstructorWithNullLogBookId() {
+        assertThrows(IllegalArgumentException.class, () -> new EmotionalLogBook(null, LOCAL_DATE_TIME, DESCRIPTION),
+                "Не получили ожидаеме исключение при вызове метода с параметром LogBookId = null");
+    }
 
-    assertThrows(IllegalArgumentException.class, () ->  new EmotionalLogBook(LOG_BOOK_ID, null, null),
-      "Не получили ожидаеме исключение при вызове метода с пустыми обязательными параметрами");
-  }
+    /**
+     * Проверяет конструктор с параметром Description = null
+     */
+    @Test
+    public void testConstructorWithNullDescription() {
+        assertThrows(IllegalArgumentException.class, () -> new EmotionalLogBook(LOG_BOOK_ID, LOCAL_DATE_TIME, null),
+                "Не получили ожидаеме исключение при вызове метода с параметром Description = null");
+    }
 
+    /**
+     * Проверяет конструктор с параметром TimeType = null
+     */
+    @Test
+    public void testConstructorWithNullTimeType() {
+        assertThrows(IllegalArgumentException.class, () -> new EmotionalLogBook(LOG_BOOK_ID, null, DESCRIPTION),
+                "Не получили ожидаеме исключение при вызове метода с параметром TimeType = null");
+    }
+
+    /**
+     * Проверяет, что конструктор обрабатывает случай с недостаточным количеством значений
+     */
+    @Test
+    public void testConstructorWithMissingRequiredValues() {
+        assertThrows(IllegalArgumentException.class, () -> new EmotionalLogBook(null, null, DESCRIPTION),
+                "Не получили ожидаеме исключение при вызове метода без всех обязательных параметров");
+
+        assertThrows(IllegalArgumentException.class, () -> new EmotionalLogBook(LOG_BOOK_ID, null, null),
+                "Не получили ожидаеме исключение при вызове метода с пустыми обязательными параметрами");
+    }
 
 
 }
