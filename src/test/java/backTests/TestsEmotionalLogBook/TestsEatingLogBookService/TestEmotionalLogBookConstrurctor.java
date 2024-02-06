08c6a413da89f8ea9bd5d934efe768db55526d64
alena.bushkina <alena.bushkina@vk.team>
2024-02-07 02:07:54 +0300

    add TestEmotionalLogBookSaveEmotionalLog

--- src/test/java/backTests/TestsEmotionalLogBook/TestsEatingLogBookService/TestEmotionalLogBookConstrurctor.java
+++
@@ -1,4 +0,0 @@
-package backTests.TestsEmotionalLogBook.TestsEatingLogBookService;
-
-public class TestEmotionalLogBookConstrurctor {
-}
--- src/test/java/backTests/TestsEmotionalLogBook/TestsEatingLogBookService/TestEmotionalLogBookSaveEmotionalLog.java
+++ src/test/java/backTests/TestsEmotionalLogBook/TestsEatingLogBookService/TestEmotionalLogBookSaveEmotionalLog.java
@@ -1,4 +1,52 @@
 package backTests.TestsEmotionalLogBook.TestsEatingLogBookService;
 
+import com.cygans.database.emotional_log_book.EmotionalLogBook;
+import com.cygans.database.emotional_log_book.EmotionalLogBookRepository;
+import com.cygans.database.emotional_log_book.EmotionalLogBookService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
 public class TestEmotionalLogBookSaveEmotionalLog {
+
+    @Mock
+    private EmotionalLogBookRepository repositoryMock;
+
+    @InjectMocks
+    private EmotionalLogBookService service;
+
+    @BeforeEach
+    void setUp() {
+        service = new EmotionalLogBookService(repositoryMock);
+    }
+
+    @Test
+    void testSaveEmotionalLog_CallsRepositorySaveMethod() {
+        EmotionalLogBook emotionalLogBook = new EmotionalLogBook();
+        service.saveEmotionalLog(emotionalLogBook);
+
+        verify(repositoryMock).save(emotionalLogBook);
+    }
+
+    @Test
+    void testSaveEmotionalLog_CallsRepositorySaveMethodOnce() {
+        EmotionalLogBook emotionalLogBook = new EmotionalLogBook();
+        service.saveEmotionalLog(emotionalLogBook);
+
+        verify(repositoryMock).save(emotionalLogBook);
+    }
+
+    @Test
+    void testSaveEmotionalLog_CallsRepositorySaveMethodWithCorrectArgument() {
+        EmotionalLogBook emotionalLogBook = new EmotionalLogBook();
+        service.saveEmotionalLog(emotionalLogBook);
+
+        verify(repositoryMock).save(emotionalLogBook);
+    }
 }
