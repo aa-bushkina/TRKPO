7f45f552ad0505365fb0055ff58227824080ac5e
alena.bushkina <alena.bushkina@vk.team>
2024-02-07 00:07:40 +0300

    add tests for TestConstructor

--- src/test/java/TestQuestion/TestConstructor.java
+++ src/test/java/TestQuestion/TestConstructor.java
@@ -1,67 +1,76 @@
 package TestQuestion;
 
-import com.cygans.database.mentor.Mentor;
 import com.cygans.database.question.Question;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.time.LocalDate;
+import java.util.stream.Stream;
 
-import static TestQuestion.Variables.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static TestQuestion.Variables.DATE;
+import static TestQuestion.Variables.PARTICIPANT_ID;
+import static TestQuestion.Variables.QUESTION;
+import static TestQuestion.Variables.STATUS_ID;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TestConstructor {
 
-  /**
-   * Проверяет конструктор с параметрами
-   */
-  @Test
-  public void testConstructorWithParameters() {
-    Question question = new Question(PARTICIPANT_ID, DATE, QUESTION, STATUS_ID);
-    assertAll(
-      () -> assertEquals(PARTICIPANT_ID, question.getParticipantId()),
-      () -> assertEquals(DATE, question.getDate()),
-      () -> assertEquals(QUESTION, question.getQuestion()),
-      () -> assertEquals(STATUS_ID, question.getStatusId())
-    );
-  }
+    /**
+     * Проверяет конструктор с параметрами
+     */
+    @Test
+    public void testConstructorWithParameters() {
+        Question question = new Question(PARTICIPANT_ID, DATE, QUESTION, STATUS_ID);
+        assertAll(
+                () -> assertEquals(PARTICIPANT_ID, question.getParticipantId()),
+                () -> assertEquals(DATE, question.getDate()),
+                () -> assertEquals(QUESTION, question.getQuestion()),
+                () -> assertEquals(STATUS_ID, question.getStatusId())
+        );
+    }
 
-  /**
-   * Проверяет конструктор без параметров
-   */
-  @Test
-  public void testDefaultConstructor() {
-    Question question = new Question();
+    /**
+     * Проверяет конструктор без параметров
+     */
+    @Test
+    public void testDefaultConstructor() {
+        Question question = new Question();
 
-    assertAll(
-      () -> assertNotNull(question.getId()),
-      () -> assertNull(question.getParticipantId()),
-      () -> assertNull(question.getQuestion()),
-      () -> assertNull(question.getStatusId()),
-      () -> assertNull(question.getAnswer()),
-      () -> assertNull(question.getDate())
-    );
-  }
+        assertAll(
+                () -> assertNotNull(question.getId()),
+                () -> assertNull(question.getParticipantId()),
+                () -> assertNull(question.getQuestion()),
+                () -> assertNull(question.getStatusId()),
+                () -> assertNull(question.getAnswer()),
+                () -> assertNull(question.getDate())
+        );
+    }
 
-  /**
-   * Проверяет конструктор с параметрами null
-   */
-  @Test
-  public void testConstructorWithNullValues() {
-    assertThrows(IllegalArgumentException.class, () -> new Question(null, null, null, null),
-      "Не получили ожидаеме исключение при вызове метода со всеми параметрами null");
-  }
+    private static Stream<Arguments> provideInvalidParams() {
+        return Stream.of(
+                Arguments.of(null, DATE, QUESTION, STATUS_ID),
+                Arguments.of(PARTICIPANT_ID, null, QUESTION, STATUS_ID),
+                Arguments.of(PARTICIPANT_ID, DATE, null, STATUS_ID),
+                Arguments.of(PARTICIPANT_ID, DATE, QUESTION, null),
+                Arguments.of(PARTICIPANT_ID, DATE, "", STATUS_ID),
+                Arguments.of(null, null, null, null)
+        );
+    }
 
-  /**
-   * Проверяет, что конструктор обрабатывает случай с недостаточным количеством значений
-   */
-  @Test
-  public void testConstructorWithMissingRequiredValues() {
-    assertThrows(IllegalArgumentException.class, () -> new Question(PARTICIPANT_ID, DATE, null, null),
-      "Не получили ожидаеме исключение при вызове метода без всех обязательных параметров");
-
-    assertThrows(IllegalArgumentException.class, () -> new Question(PARTICIPANT_ID, DATE, "", null),
-      "Не получили ожидаеме исключение при вызове метода с пустыми обязательными параметрами");
-  }
+    @ParameterizedTest(name = "[participantId: {0}, date: {1}, question: {2}, statusId: {4}")
+    @MethodSource("provideInvalidParams")
+    public void testConstructorWithMissingRequiredValues(Long participantId,
+                                                         LocalDate date,
+                                                         String question,
+                                                         Long statusId) {
+        assertThrows(IllegalArgumentException.class, () -> new Question(participantId, date, question, statusId),
+                "Не получили ожидаеме исключение при вызове метода без всех обязательных параметров");
+    }
 
 }
