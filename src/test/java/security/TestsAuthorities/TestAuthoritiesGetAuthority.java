8c4c6eda8bc25946b397976af1d146ad7200f032
alena.bushkina <alena.bushkina@vk.team>
2024-02-09 01:21:35 +0300

    add TestAuthoritiesModel

---
+++ src/test/java/security/TestsAuthorities/TestAuthoritiesGetAuthority.java
@@ -0,0 +1,47 @@
+package security.TestsAuthorities;
+
+
+import com.cygans.security.db.authorities.Authorities;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+/**
+ * Тест метода getAuthority() в Authorities
+ */
+public class TestAuthoritiesGetAuthority {
+    /**
+     * Проверяет вызов метода для объекта с authority = null
+     */
+    @Test
+    public void testGetAuthorityNull() {
+        Authorities authorities = new Authorities();
+
+        assertNull(authorities.getAuthority(), "Роль должна быть null");
+    }
+
+    /**
+     * Проверяет вызов метода для объекта с непустым значением authority
+     */
+    @Test
+    public void testGetAuthorityNotNull() {
+        String authorityValue = "ROLE_USER";
+        Authorities authorities = new Authorities();
+        authorities.setAuthority(authorityValue);
+
+        assertEquals(authorityValue, authorities.getAuthority(), "Роль должна совпадать с заданным значением");
+    }
+
+    /**
+     * Проверяет вызов метода для объекта с пустым значением authority
+     */
+    @Test
+    public void testGetAuthorityEmpty() {
+        Authorities authorities = new Authorities();
+        authorities.setAuthority("");
+
+        assertEquals("", authorities.getAuthority(), "Роль должна быть пустой строкой");
+    }
+}
+
