bb218fc58516d508de15f5fbe217dab5974affb4
alena.bushkina <alena.bushkina@vk.team>
2024-02-09 01:49:29 +0300

    add TestLoginInfoModel

---
+++ src/test/java/security/TestsLoginInfo/TestsLoginInfoModel/TestLoginInfoGetId.java
@@ -0,0 +1,35 @@
+package security.TestsLoginInfo.TestsLoginInfoModel;
+
+import com.cygans.security.db.logInfo.LoginInfo;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+/**
+ * Тест метода getId() в LoginInfo
+ */
+public class TestLoginInfoGetId {
+
+    /**
+     * Проверяет вызов метода для объекта с id = null
+     */
+    @Test
+    public void testGetIdNull() {
+        LoginInfo loginInfo = new LoginInfo();
+
+        assertNull(loginInfo.getId(), "Id должен быть null");
+    }
+
+    /**
+     * Проверяет вызов метода для объекта с установленным id
+     */
+    @Test
+    public void testGetIdNotNull() {
+        Long idValue = 1L;
+        LoginInfo loginInfo = new LoginInfo();
+        loginInfo.setId(idValue);
+
+        assertEquals(idValue, loginInfo.getId(), "Id должен совпадать с заданным значением");
+    }
+}
---
+++ src/test/java/security/TestsLoginInfo/TestsLoginInfoModel/TestLoginInfoSetPassword.java
@@ -0,0 +1,30 @@
+package security.TestsLoginInfo.TestsLoginInfoModel;
+
+import com.cygans.security.db.logInfo.LoginInfo;
+import org.junit.jupiter.api.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestLoginInfoSetPassword {
+
+    @Test
+    public void testSetPassword() {
+        String newPassword = "newPassword";
+        LoginInfo loginInfo = new LoginInfo("test@example.com", "oldPassword", 1L, (byte) 1);
+
+        loginInfo.setPassword(newPassword);
+
+        assertTrue(new BCryptPasswordEncoder().matches(newPassword, loginInfo.getPassword()), "Метод setPassword() должен устанавливать правильный пароль");
+    }
+
+    @Test
+    public void testSetEmptyPassword() {
+        String newPassword = "";
+        LoginInfo loginInfo = new LoginInfo("test@example.com", "oldPassword", 1L, (byte) 1);
+
+        loginInfo.setPassword(newPassword);
+
+        assertTrue(new BCryptPasswordEncoder().matches(newPassword, loginInfo.getPassword()), "Метод setPassword() должен корректно обрабатывать пустой пароль");
+    }
+}
