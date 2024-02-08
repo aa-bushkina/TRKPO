87254f2b027da38ffa15cf407cded18a40c50d67
alena.bushkina <alena.bushkina@vk.team>
2024-02-09 01:07:02 +0300

    add TestEatingLogBookFindAll

--- src/test/java/backTests/TestsEatingLogBook/TestsEatingLogBookService/TestEatingLogBookFindAll.java
+++ src/test/java/backTests/TestsEatingLogBook/TestsEatingLogBookService/TestEatingLogBookFindAll.java
@@ -1,6 +1,5 @@
 package backTests.TestsEatingLogBook.TestsEatingLogBookService;
 
-
 import com.cygans.database.eating_log_book.EatingLogBook;
 import com.cygans.database.eating_log_book.EatingLogBookRepository;
 import com.cygans.database.eating_log_book.EatingLogBookService;
@@ -10,8 +9,15 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class TestEatingLogBookFindAll {
@@ -23,20 +29,34 @@ public class TestEatingLogBookFindAll {
     private EatingLogBookService service;
 
     @Test
-    public void testSaveEatingLog() {
-        EatingLogBook eatingLogBookToSave = new EatingLogBook(/* передаем необходимые параметры */);
+    public void testFindAll() {
+        List<EatingLogBook> eatingLogBooksFromRepository = new ArrayList<>();
+        eatingLogBooksFromRepository.add(new EatingLogBook(1L, LocalTime.NOON, "Description 1", 1L, LocalDateTime.now()));
+        eatingLogBooksFromRepository.add(new EatingLogBook(2L, LocalTime.NOON, "Description 2", 2L, LocalDateTime.now()));
 
-        service.saveEatingLog(eatingLogBookToSave);
+        when(repository.findAll()).thenReturn(eatingLogBooksFromRepository);
 
-        verify(repository, times(1)).save(eatingLogBookToSave);
+        List<EatingLogBook> result = service.findAll();
+
+        assertEquals(eatingLogBooksFromRepository.size(), result.size());
+
+        for (int i = 0; i < eatingLogBooksFromRepository.size(); i++) {
+            EatingLogBook expectedLogBook = eatingLogBooksFromRepository.get(i);
+            EatingLogBook actualLogBook = result.get(i);
+
+            assertEquals(expectedLogBook.getLogBookId(), actualLogBook.getLogBookId());
+            assertEquals(expectedLogBook.getDescription(), actualLogBook.getDescription());
+            assertEquals(expectedLogBook.getMealId(), actualLogBook.getMealId());
+            assertEquals(expectedLogBook.getTimeEat(), actualLogBook.getTimeEat());
+        }
+
     }
 
     @Test
-    public void testSaveEatingLogSaveCalledOnce() {
-        EatingLogBook eatingLogBookToSave = new EatingLogBook(/* передаем необходимые параметры */);
+    public void testFindAllEmpty() {
+        when(repository.findAll()).thenReturn(Collections.emptyList());
+        List<EatingLogBook> result = service.findAll();
 
-        service.saveEatingLog(eatingLogBookToSave);
-
-        verify(repository, times(1)).save(eatingLogBookToSave);
+        assertTrue(result.isEmpty());
     }
 }
