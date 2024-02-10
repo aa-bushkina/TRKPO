e1fb4517f7af09402826b804d4c4fa329ae62703
stolbovik <stolbov_svyatoslav_02@mail.ru>
2024-02-10 15:01:40 +0300

    add tests for MentorSignUp2View

---
+++ src/test/java/views/TestsMentorSignUp2View/TestsSexSetUp.java
@@ -0,0 +1,53 @@
+package views.TestsMentorSignUp2View;
+
+import com.cygans.database.controllers.RegistrationAndLoginController;
+import com.cygans.views.mentor.signup.MentorSignUp2View;
+import com.vaadin.flow.component.radiobutton.RadioButtonGroup;
+import com.vaadin.flow.server.VaadinSession;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+public class TestsSexSetUp {
+
+    @Mock
+    private VaadinSession vaadinSessionMock;
+
+    @Mock
+    private RegistrationAndLoginController registrationAndLoginControllerMock;
+
+    private MentorSignUp2View mentorSignUp2View;
+
+    @BeforeEach
+    public void setUp() {
+        vaadinSessionMock.setAttribute("Gender", "Male");
+        VaadinSession.setCurrent(vaadinSessionMock);
+        mentorSignUp2View = new MentorSignUp2View(registrationAndLoginControllerMock);
+    }
+
+    /**
+     * Тест для метода sexSetUp.
+     * Проверяет, что RadioButtonGroup для пола правильно настроен.
+     */
+    @Test
+    void testSexSetUp() throws Exception {
+        Class cl = MentorSignUp2View.class;
+        Method method = cl.getDeclaredMethod("sexSetUp");
+        method.setAccessible(true);
+        method.invoke(mentorSignUp2View);
+        Field field = cl.getDeclaredField("sex");
+        field.setAccessible(true);
+
+        assertNotNull(field.get(mentorSignUp2View));
+        assertEquals("Пол", ((RadioButtonGroup<String>) field.get(mentorSignUp2View)).getLabel());
+    }
+
+}
---
+++ src/test/java/views/TestsMentorSignUp2View/TeststFormlayoutSetUp.java
@@ -0,0 +1,52 @@
+package views.TestsMentorSignUp2View;
+import com.cygans.database.controllers.RegistrationAndLoginController;
+import com.cygans.views.mentor.signup.MentorSignUp2View;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.server.VaadinSession;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+public class TeststFormlayoutSetUp {
+
+    @Mock
+    private VaadinSession vaadinSessionMock;
+
+    @Mock
+    private RegistrationAndLoginController registrationAndLoginControllerMock;
+
+    private MentorSignUp2View mentorSignUp2View;
+
+    @BeforeEach
+    public void setUp() {
+        vaadinSessionMock.setAttribute("Gender", "Male");
+        VaadinSession.setCurrent(vaadinSessionMock);
+        mentorSignUp2View = new MentorSignUp2View(registrationAndLoginControllerMock);
+    }
+
+    /**
+     * Тест для метода formlayoutSetUp.
+     * Проверяет, что форматирование элементов формы правильно установлено.
+     */
+    @Test
+    void testFormlayoutSetUp() throws Exception {
+        Class cl = MentorSignUp2View.class;
+        Method method = cl.getDeclaredMethod("formlayoutSetUp");
+        method.setAccessible(true);
+        method.invoke(mentorSignUp2View);
+        Field field = cl.getDeclaredField("formLayout");
+        field.setAccessible(true);
+
+        assertNotNull((FormLayout) field.get(mentorSignUp2View));
+        assertEquals(3, ((FormLayout) field.get(mentorSignUp2View)).getChildren().count());
+    }
+
+}
