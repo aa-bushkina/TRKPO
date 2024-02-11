761c272a2008028a59f9cd127870fe15e00a19e0
stolbovik <stolbov_svyatoslav_02@mail.ru>
2024-02-11 21:48:40 +0300

    add tests for MentorNotificationView

---
+++ src/test/java/views/TestsMentorNotificationView/TestBuildViewButton.java
@@ -0,0 +1,58 @@
+package views.TestsMentorNotificationView;
+
+import com.cygans.database.controllers.NotificationController;
+import com.cygans.database.controllers.ParticipantAndMentorController;
+import com.cygans.database.notifications.Notifications;
+import com.cygans.views.mentor.notifications.MentorNotificationView;
+import com.cygans.views.participant.notifications.ParticipantNotificationView;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.server.VaadinSession;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class TestBuildViewButton {
+
+    @Mock
+    NotificationController notificationController;
+
+    @Mock
+    ParticipantAndMentorController participantAndMentorController;
+
+    @Mock
+    UI ui;
+
+    @Mock
+    VaadinSession vaadinSession;
+
+    @InjectMocks
+    MentorNotificationView mentorNotificationView;
+
+    /**
+     * Проверка buildParticipantViewButton
+     */
+    @Test
+    void buildParticipantViewButton_ButtonBuilt_Successfully() {
+        VaadinSession.setCurrent(vaadinSession);
+        Notifications notification = new Notifications();
+        notification.setId(1L);
+        Button actualButton = mentorNotificationView.buildViewButton(notification);
+        assertEquals("Смотреть", actualButton.getText());
+        assertEquals("primary", actualButton.getThemeName());
+        UI.setCurrent(ui);
+        UI.getCurrent().access(() -> {
+            actualButton.click();
+        });
+        actualButton.click();
+    }
+
+}
