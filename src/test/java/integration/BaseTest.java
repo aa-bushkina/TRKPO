3ae289fdbbea081e395e9729216913407a659e32
alena.bushkina <alena.bushkina@vk.team>
2024-03-07 03:45:31 +0300

    add TestIntAddSportLog (not working)

---
+++ src/test/java/integration/BaseTest.java
@@ -0,0 +1,59 @@
+package integration;
+
+import com.cygans.database.log_book.LogService;
+import com.cygans.database.log_book.logs_type.LogsTypeService;
+import com.cygans.database.participant.Participant;
+import com.cygans.database.participant.ParticipantService;
+import com.cygans.security.db.logInfo.LoginInfo;
+import com.cygans.security.db.logInfo.LoginInfoService;
+import com.vaadin.flow.server.VaadinSession;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Базовый класс интеграционного теста в залогине
+ */
+@ExtendWith(MockitoExtension.class)
+public class BaseTest {
+    protected static final Logger logger = LoggerFactory.getLogger(BaseTest.class);
+    @Mock
+    protected LogService logService;
+    @Mock
+    protected LoginInfoService loginInfoService;
+
+    @Mock
+    protected LogsTypeService logsTypeService;
+    @Mock
+    protected ParticipantService participantService;
+
+    @BeforeEach
+    public void setUp() {
+        logger.info("Мокируем данные для сессии залогина");
+        Authentication authentication = new UsernamePasswordAuthenticationToken(
+                "login",
+                "password",
+                AuthorityUtils.createAuthorityList());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        VaadinSession vaadinSessionMock = mock(VaadinSession.class);
+        VaadinSession.setCurrent(vaadinSessionMock);
+        LoginInfo loginInfo = new LoginInfo();
+        loginInfo.setId(1L);
+        Participant participant = new Participant();
+        when(loginInfoService.findByLogin(any()))
+                .thenReturn(loginInfo);
+        when(participantService.getParticipantByLoginInfoId(any()))
+                .thenReturn(participant);
+    }
+}
---
+++ src/test/java/integration/TestIntAddSportLog.java
@@ -0,0 +1,68 @@
+package integration;
+
+import com.cygans.Application;
+import com.cygans.database.controllers.LogController;
+import com.cygans.database.sport_log_book.SportLogBook;
+import com.cygans.database.sport_log_book.intensity.IntensityService;
+import com.cygans.security.db.logInfo.LoginInfo;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+/**
+ * Тест проверяет, что после вызова метода контроллера добавления записи о спорте запись может быть получена
+ * при получении записей участника и по id, и все поля совпадают с установленными
+ */
+@SpringBootTest(classes = Application.class)
+@Import({IntensityService.class, LogController.class})
+public class TestIntAddSportLog extends BaseTest {
+    private static final String INTENSITY = "Низкая";
+    private static final String DURATION = "25";
+    private static final String ACTIVITY = "Утренняя зарядка";
+    private static final String COMMENTS = "Простые упражнения";
+    @Autowired
+    private LogController logController;
+    @Autowired
+    private IntensityService intensityService;
+
+    @Test
+    public void testSaveSportLogAndGetSportLogByLogbookId() {
+        logger.info("Тест проверяет, что после вызова метода контроллера добавления записи о спорте запись может " +
+                "быть получена при получении записей участника и по id, и все поля совпадают с установленными");
+
+        logger.info("Вызываем метод сохранения записи о спорте");
+        logger.info(loginInfoService.findByLogin("login").toString());
+        Long logId = logController.saveSportLog(INTENSITY, DURATION, ACTIVITY, COMMENTS);
+
+        logger.info("Проверяем, что id записи существует");
+        assertNotNull(logId, "Id записи null");
+
+        logger.info("Получаем запись о спорте по id");
+        SportLogBook retrievedSportLog = logController.getSportLogByLogbookId(logId);
+
+        logger.info("Проверяем, что запись существует");
+        assertNotNull(retrievedSportLog, "Запись, полученная по id не существует");
+
+        logger.info("Проверяем, что поля записи соответствуют ожидаемым значениям");
+        long expectedIntensity = intensityService.getIntensityId(INTENSITY); /////
+        assertAll(
+                () -> assertEquals(expectedIntensity, retrievedSportLog.getIntensityId(),
+                        "Не сопадает значение intensity"),
+                () -> assertEquals(Integer.valueOf(DURATION), retrievedSportLog.getDuration(),
+                        "Не сопадает значение duration"),
+                () -> assertEquals(ACTIVITY, retrievedSportLog.getActivity(),
+                        "Не сопадает значение activity"),
+                () -> assertEquals(COMMENTS, retrievedSportLog.getComments(),
+                        "Не сопадает значение comments")
+        );
+
+        logger.info("Тест успешно пройден");
+    }
+}
