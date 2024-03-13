ce3605fba19432bc870c3db2309464e64c619f33
alena.bushkina <alena.bushkina@vk.team>
2024-03-14 02:18:46 +0300

    add TestEnd2EndQuestionNoMentor

--- src/test/java/end2end/pages/participant/QuestionsParticipantPage.java
+++ src/test/java/end2end/pages/participant/QuestionsParticipantPage.java
@@ -3,6 +3,8 @@ package end2end.pages.participant;
 import end2end.pages.utils.PageBase;
 import org.openqa.selenium.By;
 
+import java.time.Duration;
+
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 
@@ -15,6 +17,7 @@ public class QuestionsParticipantPage extends PageBase {
     private static final By HOME_BTN = By.xpath(".//*[@icon='vaadin:home']/..");
     private static final By INPUT_AREA = By.xpath(".//vaadin-text-area");
     private static final By ASK_BTN = By.xpath(".//*[text()='Задать']");
+    private static final By NOTIFICATION = By.xpath(".//*[@id='vaadin-notification-card']");
 
     @Override
     protected void checkPage() {
@@ -31,5 +34,28 @@ public class QuestionsParticipantPage extends PageBase {
                 .shouldBe(visible.because("Нет кнопки домой"))
                 .click();
         return new StartParticipantPage();
+    }
+
+    public QuestionsParticipantPage typeQuestion(String text) {
+        logger.info("Вводим вопрос");
+        $(INPUT_AREA)
+                .shouldBe(visible.because("Нет поля ввода вопроса"))
+                .sendKeys(text);
+        return this;
+    }
+
+    public QuestionsParticipantPage clickSend() {
+        logger.info("Кликаем на кнопку отправку вопроса");
+        $(ASK_BTN)
+                .shouldBe(visible.because("Нет кнопки отправки вопроса"))
+                .click();
+        return this;
+    }
+
+    public String getNotificationText() {
+        logger.info("Получаем текст нотификации");
+        return $(NOTIFICATION)
+                .shouldBe(visible.because("Нет нотификации"), Duration.ofSeconds(5))
+                .text();
     }
 }
---
+++ src/test/java/end2end/tests/TestEnd2EndQuestionNoMentor.java
@@ -0,0 +1,37 @@
+package end2end.tests;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Тест проверяет, что нельзя задать вопрос, пока нет ментора
+ */
+public class TestEnd2EndQuestionNoMentor extends TestBase {
+    private static final String QUESTION = "вопрос";
+    private static final String NOTIFICATION_TEXT = "Чтобы задавать вопросы ментору, нужно принять его заявку";
+
+    @BeforeEach
+    public void setUp() {
+        logger.info("Регистрируем участника");
+        registerParticipant();
+    }
+
+    @Test
+    public void testEnd2EndSettingsParticipant() {
+        logger.info("Тест проверяет, что нельзя задать вопрос, пока нет ментора");
+
+        logger.info("Логинимся участником и пытаемся отправить вопрос");
+        String text = getLoginPage()
+                .login(LOGIN_PARTICIPANT, PASSWORD)
+                .andReturnStartParticipantPage()
+                .goToQuestions()
+                .typeQuestion(QUESTION)
+                .clickSend()
+                .getNotificationText();
+        assertEquals(NOTIFICATION_TEXT, text, "Текст нотификации не совпадает с ожидаемым");
+
+        logger.info("Тест прошел успешно");
+    }
+}
